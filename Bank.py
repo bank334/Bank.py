1312b6d8686af4487da9b79db90a7462ab790a75
Bank Line/BOT <bank05628@gmail.com>
2018-02-08 00:59:11 +0700

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -14,7 +14,7 @@ from threading import Thread
 import time,random,sys,json,codecs,threading,glob,urllib,urllib2,urllib3,re,ast,os,subprocess,requests,tempfile
 
 cl = LINETCR.LINE()
-cl.login(token='EpFzwpg26AP4A0huaG5e.BI7dFB6wPOVTsS82gYq53G.Alg+0c4yru0mnHOYVBfw4EvDJMZ728vH3bXoxILrVbU=')
+cl.login(token='Epvmzp4CM4hXKjsZciae.BI7dFB6wPOVTsS82gYq53G.hJnBLPBzWgBa//Ik57870SFR+GcKPMMOKDdPW6iRazk=')
 cl.loginResult()
 print "Cl-Login Success\n"
 
