68a79049dd45e712e73692102e4d216eb4859c55
Bank Line/BOT <bank05628@gmail.com>
2018-02-07 10:55:15 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,7 +20,7 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/6.0.0 iPad4,1 9.0.2"
+  UA = "Line/8.0.0"
   LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
   authToken = None
